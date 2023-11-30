8993b0d4937b91f248581e4a198027ff751aad6e
Marian Bolojan <office.adorade@gmail.com>
2023-11-30 15:38:39 +0200

    chore(release): update to release v1.1.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /*!
- * Adorade Stylelint Config (v1.1.1): .eslintrc.js
+ * Adorade Stylelint Config (v1.1.2): .eslintrc.js
  * Copyright (c) 2023 Adorade (https://github.com/adorade/stylelint-config)
  * License under MIT
  * ========================================================================== */
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * Adorade Stylelint Config (v1.1.1): index.js
+ * Adorade Stylelint Config (v1.1.2): index.js
  * Copyright (c) 2023 Adorade (https://github.com/adorade/stylelint-config)
  * License under MIT
  * ========================================================================== */
