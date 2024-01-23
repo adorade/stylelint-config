d7e1d747b86f40ac7f5c973400da0fad1d8021c3
Marian Bolojan <office.adorade@gmail.com>
2024-01-23 20:03:54 +0200

    chore(release): update to release v2.0.0

--- groups.js
+++ groups.js
@@ -1,5 +1,5 @@
 /*!
- * Adorade Stylelint Config (v1.1.2): groups.js
+ * Adorade Stylelint Config (v2.0.0): groups.js
  * Copyright (c) 2023 Adorade (https://github.com/adorade/stylelint-config)
  * License under MIT
  * ========================================================================== */
